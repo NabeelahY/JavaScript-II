8436fbb369c4198c7a3ff454d56c29e420608568
Nabeelah <nabeelahyousuph@gmail.com>
2019-05-07 13:09:33 +0100

    Complete challenges in callbacks.js file
    - implement callback functions
    - invoke function to check resultin console

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -23,28 +23,40 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   });
 
 */
-
+const cb = (arr) => {
+  console.log(arr);
+}
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, cb);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+last(items, cb);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+sumNums(2, 5, cb);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+multiplyNums(6,7,cb);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  list.indexOf(item) !== -1 ? cb(true) : cb(false);
 }
+contains('Pencil', items, cb);
 
 /* STRETCH PROBLEM */
 
